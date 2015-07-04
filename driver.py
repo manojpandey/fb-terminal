3753d2d1503866204a03b8615e93b286a7475800
bk2dcradle <ankitsultana@gmail.com>
2015-07-05 01:00:56 +0530

    Update

---
+++ driver.py
@@ -0,0 +1,22 @@
+import requests
+
+token = ''
+
+parameters = {'access_token': token}
+
+resp = requests.get('https://graph.facebook.com/v2.3/me/friends?fields=name,location', params = parameters)
+jsonData = resp.json()
+
+noLocation = []
+
+for elem in jsonData["data"]:
+	if "location" in elem:
+		print elem["name"]
+		print elem["location"]
+	else:
+		noLocation.append(elem["name"])
+
+print "Those without any location\n\n"
+
+for elem in noLocation:
+	print elem
